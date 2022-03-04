ae065d88ab0b6d020114e882625930fc50c70538
YashPame <88240082+YashPame@users.noreply.github.com>
2022-03-04 22:37:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+const getElement = (selector) => {
+  const element = document.querySelector(selector)
+
+  if (element) return element
+  throw Error(
+    `Please double check your class names, there is no ${selector} class`
+  )
+}
+
+const links = getElement('.nav-links')
+const navBtnDOM = getElement('.nav-btn')
+
+navBtnDOM.addEventListener('click', () => {
+  links.classList.toggle('show-links')
+})
+
+const date = getElement('#date')
+const currentYear = new Date().getFullYear()
+date.textContent = currentYear
